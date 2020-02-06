1a394b9c79fe8b990772a66ed68f6df09e9470ac
Assim <assassimb@gmail.com>
2020-02-06 18:21:59 -0500

    BIG FIX

--- MonInterface.h
+++ MonInterface.h
@@ -24,6 +24,7 @@ class MonInterface : public VisiTest
 public:
 	MonInterface(const char* theName);
 	void demarrer();
+	void archivecourante();
 public slots:
 	void testSuivant();
 	void arreter();
